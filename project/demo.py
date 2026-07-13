6640237fdabc7234583a494022d38288edd936a6
arpan085 <arpanbaral085@gmail.com>
2026-07-13 20:53:33 +0545

    hii

--- project/demo.py
+++ project/demo.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+from tkinter import ttk
+
 win = Tk()
 
 
@@ -14,5 +16,49 @@ win.geometry("500x500")
 Name_label = Label(win, text="Weather App",
                    font=("Time New Roman",40,"bold"))
 Name_label.place(x=25,y=50,height=50,width=450)
+
+
+list_name = [
+    "Koshi",
+    "Madhesh",
+    "Bagmati",
+    "Gandaki",
+    "Lumbini",
+    "Karnali",
+    "Sudurpashchim"
+]
+ 
+
+
+
+
+com = ttk.Combobox(win, text="Weather App",values= list_name,
+                   font=("Time New Roman",20,"bold"))
+
+com.place(x=25,y=120,height=50,width=450)
+
+
+done_button = Button(win, text="DONE",
+                   font=("Time New Roman",20,"bold"))
+
+done_button.place(x=200,y=190,height=50,width=50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 win.mainloop()
